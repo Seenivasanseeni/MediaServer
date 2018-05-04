eaa151d9b03ade3c087dbd10bd911b092cb0166e
SeenivasanSeeni <seeni0424@gmail.com>
2018-05-04 14:55:52 +0530

    TreeView Done

--- mediaServer/RemoteFileViewer/views.py
+++ mediaServer/RemoteFileViewer/views.py
@@ -11,6 +11,21 @@ def constructPath(dir):
 def get_content_type(dir):
     return "application/"+dir.split('.')[-1]
 
+def getOnlydirectories(path,dirs):
+    dirs_=[]
+    for dir in dirs:
+        dir_=os.path.join(path,dir)
+        if(os.path.isdir(dir_)):
+            dirs_.append(dir)
+    return dirs_
+
+def removeHidden(dirs):
+    dirs_=[]
+    for dir  in dirs:
+        if(len(dir)>0 and dir[0]!='.'):
+            dirs_.append(dir)
+    return dirs_
+
 def get_file_name(dir):
     name= dir.split('/')[-1]
     return name.replace(" ","%20")
@@ -18,16 +33,21 @@ def get_file_name(dir):
 def explore(request,dir="."):
     path=constructPath(dir)
     try:
-        dirs=os.listdir(path)
+        dirs_i=os.listdir(path)
+        dirs=removeHidden(dirs_i)
+        dirsOnly=getOnlydirectories(path,dirs)
     except: # if dir is a file
         with open(constructPath(dir),'rb') as file:
             response=HttpResponse(file.read())
             response['content_type']=get_content_type(dir)
             response['Content-Disposition'] = 'attachment;filename='+get_file_name(dir);
             return response
-            
+
     context={
-        'dir':dir,
+        'path':path,
+        'folder':dir,
+        'dirsOnly':dirsOnly,
         'dirs':dirs
     }
+    
     return render(request,'RemoteFileViewer/explorer.html',context=context) 
